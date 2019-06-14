0b1d7220c856df3f0ee5bfb204b9e82169abd249
zabius-technologies <zabius.technologies@gmail.com>
2019-06-14 13:40:18 +0530

    jquery fix

--- lib/footer.php
+++ lib/footer.php
@@ -627,7 +627,7 @@ This Cookie Policy is governed by and shall be construed in accordance with the 
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/bootstrap.js" ></script>
 
 
         
